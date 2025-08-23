9fc744f712081a7f2b8b6bbfcbbd460ee934d08b
Stas2056 <Stas2056@Yandex.ru>
2025-08-23 17:41:16 +0300

    add pool script

---
+++ Assets/Scripts/Pool.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+using UnityEngine.Pool;
+
+public class Pool : MonoBehaviour
+{
+    //создать пулл
+    //рандомизация спауна
+    //таймер самоуничтожения
+    //изменение цвета
+    //создать платформы
+
+    [SerializeField] private GameObject _prefab;
+    [SerializeField] private GameObject _startPoint;
+    [SerializeField] private float _repeatRate = 1f;
+    [SerializeField] private int _poolCapacity = 5;
+    [SerializeField] private int _poolMaxSize = 5;
+
+    private ObjectPool<GameObject> _pool;
+
+    private void Awake()
+    {
+        _pool = new ObjectPool<GameObject>(
+        createFunc: () => Instantiate(_prefab),
+        actionOnGet: (obj) => ActionOnGet(obj),
+        actionOnRelease: (obj) => obj.SetActive(false),
+        actionOnDestroy: (obj) => Destroy(obj),
+        collectionCheck: true,
+        defaultCapacity: _poolCapacity,
+        maxSize: _poolMaxSize);
+    }
+
+    private void ActionOnGet(GameObject obj)
+    {
+        obj.transform.position=_startPoint.transform.position;
+        obj.GetComponent<Rigidbody>().velocity = Vector3.zero;
+        obj.SetActive(true);
+    }
+
+    private void Start()
+    {
+        InvokeRepeating(nameof(GetCube), 0.0f,_repeatRate);
+    }
+
+    private void GetCube()
+    {
+        _pool.Get();
+    }
+
+}
